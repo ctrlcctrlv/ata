e898877c1964873616eccee222324a2d1dd75784
Fredrick Brennan <copypaste@kittens.ph>
2023-10-31 16:19:36 -0400

    Better docstrings

--- ata²/src/args.rs
+++ ata²/src/args.rs
@@ -1,22 +1,23 @@
-///	# ata²
-///
-///	 © 2023    Fredrick R. Brennan <copypaste@kittens.ph>
-///	 © 2023    Rik Huijzer <t.h.huijzer@rug.nl>
-///	 © 2023–   ATA Project Authors
-///
-///  Licensed under the Apache License, Version 2.0 (the "License");
-///  you may _not_ use this file except in compliance with the License.
-///  You may obtain a copy of the License at
-///
-///      http://www.apache.org/licenses/LICENSE-2.0
-///
-///  Unless required by applicable law or agreed to in writing, software
-///  distributed under the License is distributed on an "AS IS" BASIS,
-///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-///  See the License for the specific language governing permissions and
-///  limitations under the License.
-///
-/// Ask the Terminal Anything (ATA): OpenAI GPT in the terminal
+//! # ata²
+//!
+//!	 © 2023    Fredrick R. Brennan <copypaste@kittens.ph>
+//!	 © 2023    Rik Huijzer <t.h.huijzer@rug.nl>
+//!	 © 2023–   ATA Project Authors
+//!
+//!  Licensed under the Apache License, Version 2.0 (the "License");
+//!  you may _not_ use this file except in compliance with the License.
+//!  You may obtain a copy of the License at
+//!
+//!      http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!  Unless required by applicable law or agreed to in writing, software
+//!  distributed under the License is distributed on an "AS IS" BASIS,
+//!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!  See the License for the specific language governing permissions and
+//!  limitations under the License.
+//!
+//! Ask the Terminal Anything (ATA): OpenAI GPT in the terminal
+
 use crate::config::ConfigLocation;
 
 use clap::Parser;
--- ata²/src/help.rs
+++ ata²/src/help.rs
@@ -1,20 +1,21 @@
-///	# ata²
-///
-///	 © 2023    Fredrick R. Brennan <copypaste@kittens.ph>
-///	 © 2023    Rik Huijzer <t.h.huijzer@rug.nl>
-///	 © 2023–   ATA Project Authors
-///
-///  Licensed under the Apache License, Version 2.0 (the "License");
-///  you may _not_ use this file except in compliance with the License.
-///  You may obtain a copy of the License at
-///
-///      http://www.apache.org/licenses/LICENSE-2.0
-///
-///  Unless required by applicable law or agreed to in writing, software
-///  distributed under the License is distributed on an "AS IS" BASIS,
-///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-///  See the License for the specific language governing permissions and
-///  limitations under the License.
+//! # ata²
+//!
+//!	 © 2023    Fredrick R. Brennan <copypaste@kittens.ph>
+//!	 © 2023    Rik Huijzer <t.h.huijzer@rug.nl>
+//!	 © 2023–   ATA Project Authors
+//!
+//!  Licensed under the Apache License, Version 2.0 (the "License");
+//!  you may _not_ use this file except in compliance with the License.
+//!  You may obtain a copy of the License at
+//!
+//!      http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!  Unless required by applicable law or agreed to in writing, software
+//!  distributed under the License is distributed on an "AS IS" BASIS,
+//!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!  See the License for the specific language governing permissions and
+//!  limitations under the License.
+
 use rustyline::Editor;
 
 use crate::config;
