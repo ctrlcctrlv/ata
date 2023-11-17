2de1e803f2217b5ae10b62a35b776279bb72aa8e
Fredrick Brennan <copypaste@kittens.ph>
2023-11-16 22:57:20 -0500

    nightly unneeded

--- ata²/src/main.rs
+++ ata²/src/main.rs
@@ -15,9 +15,6 @@
 //!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //!  See the License for the specific language governing permissions and
 //!  limitations under the License.
-#![feature(let_chains)]
-#![feature(try_trait_v2)]
-
 #[macro_use]
 extern crate clap;
 #[macro_use]
